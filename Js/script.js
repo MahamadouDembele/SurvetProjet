eb97c008d813246c59b01668a0736bad685f5e3b
Deesmond <108226178+MahamadouDembele@users.noreply.github.com>
2022-08-06 16:03:40 +0200

    Ajout du fichier script.js

---
+++ Js/script.js
@@ -0,0 +1,9 @@
+/*=========== CHANGE LA COULEUR DE FOND ===========*/
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
